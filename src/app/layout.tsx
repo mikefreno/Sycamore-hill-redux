b8937246adb15ae20046967066e17de1f01c3753
Michael Freno <michaelt.freno@gmail.com>
2025-03-25 00:11:08 -0400

    favicon replacement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,22 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Sycamore Hill Vineyard",
   description: "A New Jersey Family Vineyard",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+    other: [
+      {
+        rel: "mask-icon",
+        url: "/safari-pinned-tab.svg",
+        color: "#84cc16",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
